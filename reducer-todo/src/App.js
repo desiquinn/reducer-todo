6153d172b852178103a5df24b960d3ec00d6a543
Desiree Morris <desiquinncompany@gmail.com>
2019-08-15 17:23:28 -0400

    set up the useReducer hook and passed in the initialState and todoReducer

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,8 +1,13 @@
-import React  from 'react';
+import React, {useReducer} from 'react';
+import {initialState, TodoReducer} from './reducers/ToDoReducer.js'
 
 import './App.css';
 
 function App() {
+
+  const [state, dispatch] = useReducer(TodoReducer, initialState);
+  // the order of the arguments matter
+  console.log('State:', state)
   return (
     <div className="App">
     
