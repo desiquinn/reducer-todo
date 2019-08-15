6de7594fc3c50f099dbfae1fda4950c51eb2cfb7
Desiree Morris <desiquinncompany@gmail.com>
2019-08-15 17:06:37 -0400

    cleaned up the format of my reducer by putting it inside an array within the state object

---
+++ reducer-todo/src/reducers/ToDoReducer.js
@@ -0,0 +1,23 @@
+
+// exporting intial state which needs to be an object, but we want to add items
+// so it needs to be an array inside the object, with an object for each item
+// initial state has to be an object so that it can contain all of the shared state
+// that our application needs to funtion.  It's easier to just add another key, than
+// adding to an array. Its able to represent different types of data.  State is an object.
+
+export const initialState = {
+    todoList: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        }
+    ]
+};
+
+export const TodoReducer = (state, action) => {
+    switch (action.type){
+        default:
+        return state;
+    }
+};
